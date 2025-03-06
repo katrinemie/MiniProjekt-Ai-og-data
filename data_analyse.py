daf44ce0aa08abe20cd8f746fba93951ce75d6ce
Katrine <katrine.mie.thomsen@gmail.com>
2025-03-06 14:28:28 +0100

    ny miaw

--- data_analyse.py
+++ data_analyse.py
@@ -1,5 +1,17 @@
-import pandas as pd
+import pandas as pd  
 
-file_name = ("Medical_Students.csv")
 
+file_name = "medical_students_dataset.csv"  
 df = pd.read_csv(file_name)
+
+#5 rækker
+print("Første 5 rækker af datasættet:")
+print(df.head())
+
+
+print("\nInformation om datasættet:")
+print(df.info())
+
+
+print("\nManglende værdier pr. kolonne:")
+print(df.isnull().sum())
