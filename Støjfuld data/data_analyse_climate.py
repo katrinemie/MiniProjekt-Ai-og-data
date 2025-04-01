7e0b42eb8711c6e0d39c87a3894ca633bc8f5212
Katrine <katrine.mie.thomsen@gmail.com>
2025-04-01 12:53:17 +0200

    Climate dataset

---
+++ Støjfuld data/data_analyse_climate.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\katri\Documents\2 semester\Ai og data\MiniProjekt-Ai-og-data\MiniProjekt-Ai-og-data\Støjfuld data\DailyDelhiClimateTrain.csv")
+
+
+
+print(df.head())
+
+
+print(df.info())
