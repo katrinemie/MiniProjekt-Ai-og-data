45f65aa47c386eac12ff3fbab0affd3ba18104b5
Katrine <katrine.mie.thomsen@gmail.com>
2025-04-14 11:36:33 +0200

    Last one

--- Støjfuld data/moving_temp.py
+++ Støjfuld data/moving_temp.py
@@ -6,12 +6,12 @@ filnavn = r"Støjfuld data\DailyDelhiClimateTrain.csv"
 df = pd.read_csv(filnavn, parse_dates=["date"])
 
 
-df["glidende_gns"] = df["meantemp"].rolling(window=7).mean()
+df["movig_gns"] = df["meantemp"].rolling(window=7).mean()
 
 #Plot :)
 plt.figure(figsize=(15, 6))
 plt.plot(df["date"], df["meantemp"], label="Original temperatur", alpha=0.5)
-plt.plot(df["date"], df["glidende_gns"], label="Moving avarage (7 dage)", color="red")
+plt.plot(df["date"], df["moving_gns"], label="Moving avarage (7 dage)", color="red")
 plt.title("Temperatur over tid, Moving avarage")
 plt.xlabel("Dato")
 plt.ylabel("Temperatur (°C)")
